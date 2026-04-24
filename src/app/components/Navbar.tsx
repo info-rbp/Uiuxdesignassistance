bf0c5434d1b2a6d018e802394bceba746031387d
info-rbp <info@remotebusinesspartner.com.au>
2026-04-24 07:54:23 -0700

    Rename offers navigation label

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,7 +10,7 @@ const secondaryLinks = [
   { label: "Services Hub", href: "/services" },
   { label: "Document Nucleus", href: "/docushare" },
   { label: "Application Launchpad", href: "/applications" },
-  { label: "Offers Gateway", href: "/offers" },
+  { label: "Offers & Deals", href: "/offers" },
   { label: "Finance Center", href: "/finance" },
   { label: "Resource Base", href: "/resources" },
   { label: "Membership Central", href: "/membership" },
